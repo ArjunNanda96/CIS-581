84f2a82b85337f1695d299bad60639b17882ba82
hillcj1 <hillcj@seas.upenn.edu>
2021-09-27 18:21:55 -0400

    Update NMS.py

--- NMS.py
+++ NMS.py
@@ -1,52 +1,72 @@
-def edgeLink(M, Mag, Ori, low, high):
+from numpy.lib.function_base import interp
+def nonMaxSup(Mag, Ori):
     '''
     File clarification:
-        Use hysteresis to link edges based on high and low magnitude thresholds
-        - Input M: H x W logical map after non-max suppression
-        - Input Mag: H x W matrix represents the magnitude of gradient
-        - Input Ori: H x W matrix represents the orientation of gradient
-        - Input low, high: low and high thresholds 
-        - Output E: H x W binary matrix represents the final canny edge detection map
+        Find local maximum edge pixel using NMS along the line of the gradient
+        - Input Mag: H x W matrix represents the magnitude of derivatives
+        - Input Ori: H x W matrix represents the orientation of derivatives
+        - Output M: H x W binary matrix represents the edge map after non-maximum suppression
     '''
-
-    strong_map = np.multiply(M, np.greater(Mag, high))
-    weak_map = np.multiply(M, np.logical_and(Mag > low, Mag <= high))
-
-    # compute the edge direction from Ori
-    edge_dir = Ori + (np.pi / 2)
+    """
     nc, nr = Ori.shape[1], Ori.shape[0]
     x, y = np.meshgrid(np.arange(nc), np.arange(nr))
+    
+    y_down = np.clip(y + 1, 0, nr - 1) 
+    y_up = np.clip(y - 1, 0, nr - 1)
+    x_right = np.clip(x + 1, 0, nc - 1)
+    x_left = np.clip(x - 1, 0, nc - 1)
 
-    cos_map = np.cos(edge_dir) + x
-    #print(np.cos(edge_dir))
+    cos_map = np.cos(Ori) + x_right
+    sin_map = np.sin(Ori) + y_up
+
+    neg_Ori = Ori + np.pi
+    cos_map_neg = np.cos(neg_Ori) + x_left
+    sin_map_neg = np.sin(neg_Ori) + y_down
+
+    # using interpolation to get neighbor
+
+
+    #check to see if they are in bounds
+    neighbor1 = interp2(Mag, cos_map, sin_map)
+    neighbor2 = interp2(Mag, cos_map_neg, sin_map_neg)
+    print(neighbor1)
+    print(neighbor2)
+
+    # getting neighbor in the opposite of the oritention direction
+    
+    binary_map = np.logical_and(Mag >= neighbor1, Mag >= neighbor2)
+    return binary_map
+    """
+    nc, nr = Ori.shape[1], Ori.shape[0]
+    x, y = np.meshgrid(np.arange(nc), np.arange(nr))
+    
+    y_down = np.clip(y + 1, 0, nr - 1) 
+    y_up = np.clip(y - 1, 0, nr - 1)
+    x_right = np.clip(x + 1, 0, nc - 1)
+    x_left = np.clip(x - 1, 0, nc - 1)
+
+    cos_map = np.cos(Ori) + x
     print(cos_map)
-    cos_map = np.clip(x + 1, 0, nc - 1)
-    print(cos_map)
-    sin_map = np.sin(edge_dir)
-    sin_map = np.clip(y - 1, 0, nr - 1)
+    cos_map_oob = np.logical_and(np.around(cos_map, 5) >= 0, cos_map <= (nc - 1))
+    sin_map = np.sin(Ori) + y
+    print(sin_map)
+    sin_map_oob = np.logical_and(np.around(sin_map, 5) >= 0, sin_map <= (nr - 1))
+    forward_oob = np.logical_and(sin_map_oob, cos_map_oob)
+    neighbor1 = interp2(Mag, cos_map, sin_map)
+    print(neighbor1)
+    N1_trim = np.multiply(neighbor1, forward_oob)
+    print(N1_trim)
 
-    neg_edge_dir = Ori - (np.pi / 2)
-    cos_map_neg = np.cos(neg_edge_dir)
-    cos_map_neg = np.clip(x - 1, 0, nc - 1)
-    sin_map_neg = np.sin(neg_edge_dir)
-    sin_map_neg = np.clip(y + 1, 0, nr - 1) 
+    cos_map_neg = np.cos(Ori + np.pi) + x
+    cos_map_neg_oob = np.logical_and(np.around(cos_map_neg, 5) >= 0, cos_map_neg <= (nc - 1))
+    sin_map_neg = np.sin(Ori + np.pi) + y
+    sin_map_neg_oob = np.logical_and(np.around(sin_map_neg, 5) >= 0, sin_map_neg <= (nr - 1))
+    backwards_oob = np.logical_and(sin_map_neg_oob, cos_map_neg_oob)
+    neighbor2 = interp2(Mag, cos_map_neg, sin_map_neg)
+    N2_trim = np.multiply(neighbor2, backwards_oob)
 
-    xf = np.multiply(cos_map, weak_map)
-    xb = np.multiply(cos_map_neg, weak_map)
-    yf = np.multiply(sin_map, weak_map)
-    yb = np.multiply(sin_map_neg, weak_map)
+    #print(N1_trim)
+    #print(N2_trim)
 
-    prev_map = np.copy(strong_map)
-    count = 0
-    while True:
-        count += 1
-        N1 = interp2(strong_map, xf, yf)
-        N2 = interp2(strong_map, xb, yb)
-        strong_map = np.logical_or(strong_map, N1)
-        strong_map = np.logical_or(strong_map, N2)
-        cur_map = np.copy(strong_map)
-        if np.array_equal(prev_map, cur_map):
-            #print(count)
-            return strong_map
-        else:
-            prev_map = np.copy(strong_map)
+    binary_map = np.logical_and(Mag >= N1_trim, Mag >= N2_trim)
+    return binary_map
